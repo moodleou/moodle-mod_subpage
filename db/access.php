moodleou/moodle-mod_forumng

Add the new :addinstance capability to all OU mods. #3231

db/access.php
@@ -37,6 +37,19 @@ $capabilities = array(
         )
     ),
 
+    // Ability to add new forumng to a course
+    'mod/forumng:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
+
     'mod/forumng:viewdiscussion' => array(
 
         'captype' => 'read',

moodleou/moodle-mod_subpage

db/access.php
@@ -37,4 +37,17 @@ $capabilities = array(
             'user' => CAP_ALLOW
         )
     ),
+
+    // Ability to add new subpage to a course
+    'mod/subpage:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
 );